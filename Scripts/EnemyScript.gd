e9962a981387436f74fe4e7bf15460b50b576278
Dovodx <45951326+Dovodx@users.noreply.github.com>
2024-04-13 11:40:18 -0400

    Summon, kill, adding sfx

--- Scripts/EnemyScript.gd
+++ Scripts/EnemyScript.gd
@@ -9,6 +9,9 @@ var sprite: Sprite2D
 var spritePositions = [Vector2.ZERO, Vector2.ZERO]
 var lastPhysTime = Time.get_ticks_usec()
 
+var hitbox: Area2D
+var hurtbox: Area2D
+
 func _ready():
 	movementTimer = get_node("movement timer")
 	movementTimer.start()
@@ -17,6 +20,14 @@ func _ready():
 	sprite = get_node("sprite")
 	spritePositions[0] = sprite.global_position
 	spritePositions[1] = sprite.global_position
+	
+	hitbox = get_node("hitbox")
+	hitbox.set_deferred("monitoring", true)
+	hitbox.set_deferred("monitorable", true)
+	hurtbox = get_node("hurtbox")
+	hitbox.set_deferred("monitoring", true)
+	hitbox.set_deferred("monitorable", true)
+	visible = true
 
 func _process(delta):
 	var timeDiff = (Time.get_ticks_usec() - lastPhysTime) / 1000000.0
@@ -26,9 +37,8 @@ func _process(delta):
 	)
 
 func _physics_process(delta):
-	var movementVector = Vector2.ZERO
-	#todo where to move
 	velocity *= 0.98
+	sprite.flip_h = velocity.x > 0
 
 	move_and_slide()
 	
@@ -39,3 +49,11 @@ func _physics_process(delta):
 
 func _on_movement_timer_timeout():
 	velocity = (player.global_position - global_position).normalized() * SPEED
+
+func die():
+	hitbox.set_deferred("monitoring", false)
+	hitbox.set_deferred("monitorable", false)
+	hurtbox.set_deferred("monitoring", false)
+	hurtbox.set_deferred("monitorable", false)
+	visible = false
+	get_node("die").play()
--- Scripts/PlayerScript.gd
+++ Scripts/PlayerScript.gd
@@ -7,8 +7,13 @@ var health = 100
 var invulnerable = false
 var invulnTimer: Timer
 
-var sprite: Sprite2D
+var crystalsCollected = 0
+var summonActive = false
+
+var spriteRoot: Node2D
 var spritePositions = [Vector2.ZERO, Vector2.ZERO]
+var charSprite: Sprite2D
+var summonSprite: Sprite2D
 var lastPhysTime = Time.get_ticks_usec()
 
 var anim: AnimationPlayer
@@ -20,16 +25,18 @@ func _ready():
 	healthBar = get_node("/root/level/Control/health fill")
 	invulnTimer = get_node("invuln timer")
 	
-	sprite = get_node("sprite")
-	spritePositions[0] = sprite.global_position
-	spritePositions[1] = sprite.global_position
+	spriteRoot = get_node("sprite root")
+	spritePositions[0] = spriteRoot.global_position
+	spritePositions[1] = spriteRoot.global_position
+	charSprite = get_node("sprite root/sprite")
+	summonSprite = get_node("sprite root/summon sprite")
 	anim = get_node("AnimationPlayer")
 	anim.play("stand")
 
 func _process(delta):
 	var timeDiff = (Time.get_ticks_usec() - lastPhysTime) / 1000000.0
 	var lerpWeight = timeDiff / (1.0 / Engine.physics_ticks_per_second)
-	sprite.global_position = spritePositions[0].lerp(
+	spriteRoot.global_position = spritePositions[0].lerp(
 		spritePositions[1], lerpWeight
 	)
 
@@ -40,7 +47,8 @@ func _physics_process(delta):
 		movementVector.x = dirH * SPEED
 	else:
 		movementVector.x = dirH * -SPEED
-	sprite.flip_h = sprite.flip_h if dirH == 0 else dirH < 0
+	charSprite.flip_h = charSprite.flip_h if dirH == 0 else dirH < 0
+	summonSprite.flip_h = summonSprite.flip_h if dirH == 0 else dirH < 0
 	
 	var dirV = Input.get_axis("up", "down")
 	if dirV:
@@ -49,6 +57,11 @@ func _physics_process(delta):
 		movementVector.y = dirV * -SPEED
 	
 	velocity = movementVector.limit_length(SPEED)
+	if !summonActive:
+		if movementVector.length() == 0:
+			anim.play("stand")
+		else:
+			anim.play("run")
 
 	move_and_slide()
 	
@@ -58,20 +71,42 @@ func _physics_process(delta):
 
 func get_crystal(crystal):
 	crystal.visible = false
+	crystal.set_deferred("monitoring", false)
+	crystal.set_deferred("monitorable", false)
+	crystalsCollected += 1
+	get_node("sounds/pickup").play()
+	if !summonActive and crystalsCollected == 3:
+		summon()
 	#todo play sound
+	#todo get beeg friend when you get all crystals, respawn crystals, etc
 
+func summon():
+	print("summon active")
+	#todo stop movement during summon probably
+	summonActive = true
+	invulnTimer.stop()
+	invulnerable = true
+	anim.play("summon")
+	anim.queue("summon_run")
+	#todo end state when all enemies gone
 
 func _on_hitbox_body_entered(body):
 	take_damage(10)
 
 func take_damage(dmg):
+	if invulnerable: return
 	invulnerable = true
 	invulnTimer.start()
 	health -= dmg
 	healthBar.size.x = max((health as float / maxHealth) * 180.0, 0)
 	#todo death, hurt anim
 
-
 func _on_invuln_timer_timeout():
 	#todo invuln effect
 	invulnerable = false
+
+
+func _on_enemy_hit(area):
+	#todo score, sfx, track when all enemies dead
+	if area.get_parent().get_parent().name == "enemies":
+		area.get_parent().die()
